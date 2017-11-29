2821e3e71271b0e598ff5b92097ba8c0ac9f8255
Owen Vey <owen.vey@gmail.com>
2017-11-28 22:58:38 -0600

    created fragments for each tab and implemented state saving

--- app/src/main/java/visualbasics/petsavers/MainActivity.java
+++ app/src/main/java/visualbasics/petsavers/MainActivity.java
@@ -3,42 +3,103 @@ package visualbasics.petsavers;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.MotionEvent;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    BottomNavigationView bottomNavigationView;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+    //This is our viewPager
+    private ViewPager viewPager;
 
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_search:
-                    mTextMessage.setText(R.string.title_search);
-                    return true;
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_profile:
-                    mTextMessage.setText(R.string.title_profile);
-                    return true;
-            }
-            return false;
-        }
-    };
+    //Fragments
+    SearchFragment searchFragment;
+    HomeFragment homeFragment;
+    ProfileFragment profileFragment;
+    MenuItem prevMenuItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = findViewById(R.id.message);
-        BottomNavigationView navigation = findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        //Initializing viewPager
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+
+        //Initializing the bottomNavigationView
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.navigation_search:
+                                viewPager.setCurrentItem(0, false);
+                                break;
+                            case R.id.navigation_home:
+                                viewPager.setCurrentItem(1, false);
+                                break;
+                            case R.id.navigation_profile:
+                                viewPager.setCurrentItem(2, false);
+                                break;
+                        }
+                        return false;
+                    }
+                });
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (prevMenuItem != null) {
+                    prevMenuItem.setChecked(false);
+                } else {
+                    bottomNavigationView.getMenu().getItem(0).setChecked(false);
+                }
+                bottomNavigationView.getMenu().getItem(position).setChecked(true);
+                prevMenuItem = bottomNavigationView.getMenu().getItem(position);
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        //Disable ViewPager Swipe
+        viewPager.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return true;
+            }
+        });
+
+        setupViewPager(viewPager);
+        bottomNavigationView.setSelectedItemId(R.id.navigation_home);
     }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        searchFragment = new SearchFragment();
+        homeFragment = new HomeFragment();
+        profileFragment = new ProfileFragment();
+        adapter.addFragment(searchFragment);
+        adapter.addFragment(homeFragment);
+        adapter.addFragment(profileFragment);
+        viewPager.setAdapter(adapter);
+    }
+
 
 }
---
+++ app/src/main/java/visualbasics/petsavers/ProfileFragment.java
@@ -0,0 +1,25 @@
+package visualbasics.petsavers;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ProfileFragment extends Fragment {
+
+    public static ProfileFragment newInstance() {
+        ProfileFragment fragment = new ProfileFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+}
