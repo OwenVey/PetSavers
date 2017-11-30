a6230bf250ce5cb109f17af1600ba254890332f7
Owen Vey <owen.vey@gmail.com>
2017-11-29 23:13:25 -0600

    added database and dog object

---
+++ app/src/main/java/visualbasics/petsavers/AppDatabase.java
@@ -0,0 +1,26 @@
+package visualbasics.petsavers;
+
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+
+@Database(entities = {Dog.class, Cat.class}, version = 1)
+public abstract class AppDatabase extends RoomDatabase {
+
+    private static AppDatabase INSTANCE;
+
+    public abstract DogDao dogDao();
+    public abstract CatDao catDao();
+
+    public static AppDatabase getAppDatabase(Context context) {
+        if (INSTANCE == null) {
+            INSTANCE = Room.databaseBuilder(context.getApplicationContext(), AppDatabase.class, "database").build();
+        }
+        return INSTANCE;
+    }
+
+    public static void destroyInstance() {
+        INSTANCE = null;
+    }
+}
