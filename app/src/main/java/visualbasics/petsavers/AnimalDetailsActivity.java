f0e011c75f256c8957d552fd045a4e6ffb12d93c
Owen Vey <owen.vey@gmail.com>
2017-12-02 15:23:23 -0600

    added activity for when the user clicks on an animal in a search

---
+++ app/src/main/java/visualbasics/petsavers/AnimalDetailsActivity.java
@@ -0,0 +1,42 @@
+package visualbasics.petsavers;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import java.util.List;
+
+public class AnimalDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_animal_details);
+
+        int animalId = getIntent().getIntExtra("animalId", -1);
+        new FindAnimalById(this, animalId).execute();
+    }
+
+    private class FindAnimalById extends AsyncTask<Void, Void, Animal> {
+
+        private Context context;
+        int animalId;
+
+        public FindAnimalById (Context context, int animalId){
+            this.context = context;
+            this.animalId = animalId;
+        }
+
+        @Override
+        protected Animal doInBackground(Void... params) {
+            AnimalDao animalDao = AppDatabase.get(context).animalDao();
+            return animalDao.findById(animalId);
+        }
+
+        @Override
+        protected void onPostExecute(Animal animal) {
+            setTitle(animal.name);
+        }
+    }
+}
